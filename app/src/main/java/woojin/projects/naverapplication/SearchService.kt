8a5f5756fdd3f89892b3999e14746607300b24b0
Woojin <woojin9949@gmail.com>
2023-08-15 18:19:38 +0900

    update

--- app/src/main/java/woojin/projects/naverapplication/SearchService.kt
+++ app/src/main/java/woojin/projects/naverapplication/SearchService.kt
@@ -2,11 +2,14 @@ package woojin.projects.naverapplication
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface SearchService {
     @GET("v1/search/local.json")
     fun getMustGoRestaurants(
+//        @Header("X-Naver-Client-Id") clientId:String,
+//        @Header("X-Naver-Clent-Secret") clientSecret:String,
         @Query("query") query: String,
         @Query("display") display: Int
     ): Call<SearchResult>
